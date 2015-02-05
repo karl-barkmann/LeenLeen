30016104804940d051ab77ea72aaeade7db896b4
karl-barkmann <karlassassin@hotmail.com>
2015-02-05 18:24:15 +0800

    添加一些加载控件。

---
+++ Smart.Windows.Controls/BusyAdorner.cs
@@ -0,0 +1,52 @@
+﻿using Smart.Windows.Controls;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace Smart.Windows.Documents
+{
+    public class BusyAdorner : Adorner
+    {
+        public static readonly RoutedEvent CancelEvent =
+            EventManager.RegisterRoutedEvent("Cancel", RoutingStrategy.Direct, typeof(RoutedEventHandler), typeof(BusyAdorner));
+
+        private BusyIndicator busyIndicator;
+
+        public BusyAdorner(UIElement adornedElement)
+            : base(adornedElement)
+        {
+            IsHitTestVisible = true;
+
+            busyIndicator = new BusyIndicator();
+            busyIndicator.DataContext = adornedElement;
+            AddVisualChild(busyIndicator);
+        }
+
+        public event RoutedEventHandler Cancel
+        {
+            add { AddHandler(CancelEvent, value); }
+            remove { RemoveHandler(CancelEvent, value); }
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get { return 1; }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return busyIndicator;
+        }
+
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            busyIndicator.Arrange(new Rect(arrangeBounds));
+            return arrangeBounds;
+        }
+
+        internal void FireCancel()
+        {
+            RaiseEvent(new RoutedEventArgs(CancelEvent, this));
+        }
+    }
+}
