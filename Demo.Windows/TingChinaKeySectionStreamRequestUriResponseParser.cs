3d9a1b245e9c4b452674f4577a8ff3312aa54d85
karl-barkmann <karlassassin@hotmail.com>
2015-05-27 17:46:10 +0800

    手动解决冲突。
    手动解决冲突。

--- Demo.Windows/TingChinaKeySectionStreamRequestUriResponseParser.cs
+++ Demo.Windows/TingChinaKeySectionStreamRequestUriResponseParser.cs
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Demo.Windows
-{
-    class TingChinaKeySectionStreamRequestUriResponseParser : IKeySectionStreamRequstUriResponseParser
-    {
-        public string GetKey(string rawHtmlContent)
-        {
-            var keyIndex = "url[3]=";
-            var realUrlIndex = rawHtmlContent.IndexOf(keyIndex);
-            var realUrlIndex1 = rawHtmlContent.IndexOf(";", realUrlIndex);
-
-            var realUrl = rawHtmlContent.Substring(realUrlIndex + keyIndex.Length + 2, realUrlIndex1 - realUrlIndex - keyIndex.Length - 3);
-            string decodeUrl = System.Web.HttpUtility.UrlDecode(realUrl);
-
-            var uri = new Uri(decodeUrl);
-
-            return uri.Query.Remove(0, 4);
-        }
-    }
-}
-=======
 using Demo.Windows;
 using System;
 using System.Net;
@@ -43,4 +16,3 @@ public class TingChinaKeySectionStreamRequestUriResponseParser : IKeySectionStre
         return uri.Query.Substring(3, uri.Query.Length);
     }
 }
->>>>>>> origin/master
