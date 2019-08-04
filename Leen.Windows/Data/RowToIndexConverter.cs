b06507063f578afebb50a65ac12478ddb83bd8f7
李平 <karlassassin@hotmail.com>
2019-08-04 19:28:20 +0800

    增加一个用于自动生成行编号的值转换器。

---
+++ Leen.Windows/Data/RowToIndexConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Leen.Windows.Data
+{
+    /// <summary>
+    /// 自动生成行数字的值转换器。
+    /// </summary>
+    public class RowToIndexConverter : MarkupExtension, IValueConverter
+    {
+        static RowToIndexConverter converter;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is DataGridRow row)
+                return row.GetIndex() + 1;
+            else
+                return -1;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (converter == null) converter = new RowToIndexConverter();
+            return converter;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public RowToIndexConverter()
+        {
+        }
+    }
+}
