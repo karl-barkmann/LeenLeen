f5b08edb078d53ef90dcf6b24405e48e6f03921d
karl <karlassassin@hotmail.com>
2020-12-20 22:45:20 +0800

    crawler strategy add.

--- Demo.Windows/ViewModel/WeixinArticleCrawlerViewModel.cs
+++ Demo.Windows/ViewModel/WeixinArticleCrawlerViewModel.cs
@@ -3,6 +3,7 @@ using Abot.Poco;
 using AngleSharp.Parser.Html;
 using Leen.Practices.Mvvm;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Net;
@@ -24,7 +25,7 @@ namespace Demo.Windows.ViewModel
 
         public WeixinArticleCrawlerViewModel()
         {
-            CrawlCommand = new RelayCommand(Crawl);
+            CrawlCommand = new RelayCommand(Crawl1);
             StopCrawlingCommand = new RelayCommand(StopCrawling);
             BindingOperations.EnableCollectionSynchronization(_articles, lockObject);
             BindingOperations.EnableCollectionSynchronization(crawlingLogs, logslockObject);
@@ -74,8 +75,13 @@ namespace Demo.Windows.ViewModel
             }
         }
 
+        const string directory = "Articles";
+        const string crawlTarget = "https://wemp.app/accounts/45bd407b-dcea-4be6-8e8f-7f39c975b448";
+
         private void Crawl()
         {
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
             ThreadPool.QueueUserWorkItem((state) =>
             {
                 CrawlConfiguration crawlConfig = new CrawlConfiguration();
@@ -87,25 +93,25 @@ namespace Demo.Windows.ViewModel
                 crawler.PageCrawlCompletedAsync += crawler_ProcessPageCrawlCompleted;
                 crawler.PageCrawlDisallowedAsync += crawler_PageCrawlDisallowed;
                 crawler.PageLinksCrawlDisallowedAsync += crawler_PageLinksCrawlDisallowed;
+
                 crawler.ShouldCrawlPage((crawledPage, crawledContext) =>
                 {
                     CrawlDecision decision = new CrawlDecision();
                     var uri = crawledPage.Uri.ToString();
-                    var index = crawledPage.Uri.Query.Replace("?page=", "");
-                    if (crawledPage.IsRoot || (uri.StartsWith("https://wemp.app/accounts/45bd407b-dcea-4be6-8e8f-7f39c975b448?page=") && int.Parse(index) >= 9) ||
-                    uri.StartsWith("https://wemp.app/posts/"))
+                    var parentUri = crawledPage.ParentUri.ToString();
+                    if (crawledPage.IsRoot || uri.StartsWith(crawlTarget + "?page=") || (uri.StartsWith("https://wemp.app/posts/") && parentUri.StartsWith(crawlTarget)))
                     {
                         decision.Allow = true;
                     }
                     else
                     {
                         decision.Allow = false;
-                        decision.Reason = "Just skip pages!";
+                        decision.Reason = "skip pages";
                     }
                     return decision;
                 });
 
-                CrawlResult result = crawler.Crawl(new Uri("https://wemp.app/accounts/45bd407b-dcea-4be6-8e8f-7f39c975b448?page=9"), cancellationTokenSource);
+                CrawlResult result = crawler.Crawl(new Uri(crawlTarget), cancellationTokenSource);
 
                 if (result.ErrorOccurred)
                 {
@@ -113,9 +119,66 @@ namespace Demo.Windows.ViewModel
                     AddLog(string.Format("Crawl of {0} completed with error: {1}", result.RootUri.AbsoluteUri, result.ErrorException.Message));
                 }
                 else
+                {
                     AddLog(string.Format("Crawl of {0} completed without error.", result.RootUri.AbsoluteUri));
-                Console.ReadLine();
+                }
             }, null);
+        }
+
+        private void Crawl1()
+        {
+            var candidateIndexes = new List<string> { "2", "3", "7", "9", "11", "12", "13", "16", "17", "23", "24", "25", "27", "29", "30", "31", "32", "33", "34", "35", "38", "40" };
+            ThreadPool.QueueUserWorkItem((state) =>
+            {
+                CrawTarget(crawlTarget);
+                foreach (var index in candidateIndexes)
+                {
+                    var crawlingPage = crawlTarget + "?page=" + index;
+                    CrawTarget(crawlingPage);
+                }
+            });
+        }
+
+        private void CrawTarget(string crawlingPage)
+        {
+            var candidateIndexes = new List<string> { "2", "3", "7", "9", "11", "12", "13", "16", "17", "23", "24", "25", "27", "29", "30", "31", "32", "33", "34", "35", "38", "40" };
+            CrawlConfiguration crawlConfig = new CrawlConfiguration();
+            crawlConfig.MaxConcurrentThreads = 10;
+            crawlConfig.MaxPagesToCrawl = 10000;
+            crawlConfig.CrawlTimeoutSeconds = 500;
+            crawlConfig.HttpRequestTimeoutInSeconds = 500;
+            crawlConfig.UserAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36";
+            PoliteWebCrawler crawler = new PoliteWebCrawler(crawlConfig, null, null, null, null, null, null, null, null);
+            crawler.PageCrawlStartingAsync += crawler_ProcessPageCrawlStarting;
+            crawler.PageCrawlDisallowedAsync += crawler_PageCrawlDisallowed;
+            crawler.PageCrawlCompletedAsync += crawler_ProcessPageCrawlCompleted;
+            crawler.PageLinksCrawlDisallowedAsync += crawler_PageLinksCrawlDisallowed; crawler.ShouldCrawlPage((crawledPage, crawledContext) =>
+            {
+                CrawlDecision decision = new CrawlDecision();
+                var uri = crawledPage.Uri.ToString();
+                var parentUri = crawledPage.ParentUri.ToString();
+                var index = crawledPage.Uri.Query.Replace("?page=", "").Trim();
+
+                if (crawledPage.IsRoot || (uri.StartsWith("https://wemp.app/posts/") && parentUri == crawlingPage))
+                {
+                    decision.Allow = true;
+                }
+                else
+                {
+                    decision.Allow = false;
+                    decision.Reason = "skip pages";
+                }
+                return decision;
+            });
+            var result = crawler.Crawl(new Uri(crawlingPage), cancellationTokenSource);
+            if (result.ErrorOccurred)
+            {
+                AddLog(string.Format("Page {0} crawling completed with error: {1}", result.RootUri.AbsoluteUri, result.ErrorException.Message));
+            }
+            else
+            {
+                AddLog(string.Format("Page {0} crawling completed without error.", result.RootUri.AbsoluteUri));
+            }
         }
 
         void crawler_ProcessPageCrawlStarting(object sender, PageCrawlStartingArgs e)
@@ -128,31 +191,39 @@ namespace Demo.Windows.ViewModel
         void crawler_ProcessPageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
         {
             CrawledPage crawledPage = e.CrawledPage;
-
-            if (crawledPage.WebException != null || crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK)
+            if (crawledPage.WebException != null)
             {
-                AddLog(string.Format("Crawl of page failed {0}", crawledPage.Uri.AbsoluteUri));
+                AddLog(string.Format("Page {0} crawling failed {1}",
+                    crawledPage.Uri.AbsoluteUri,
+                    crawledPage.WebException.InnerException != null ? crawledPage.WebException.InnerException.Message : crawledPage.WebException.Message));
+                return;
+            }
+            else if (crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK)
+            {
+                AddLog(string.Format("Page {0} crawling failed {1}", crawledPage.Uri.AbsoluteUri, crawledPage.HttpWebResponse.StatusCode));
                 return;
             }
             else if (string.IsNullOrEmpty(crawledPage.Content.Text))
             {
-                AddLog(string.Format("Page had no content {0}", crawledPage.Uri.AbsoluteUri));
+                AddLog(string.Format("Page {0} has no content", crawledPage.Uri.AbsoluteUri));
                 return;
             }
 
-            AddLog(string.Format("Crawl of page succeeded {0}", crawledPage.Uri.AbsoluteUri));
+            AddLog(string.Format("Page {0} crawling succeeded", crawledPage.Uri.AbsoluteUri));
+
             if (crawledPage.IsRoot)
                 return;
 
-            var query = crawledPage.AngleSharpHtmlDocument.QuerySelector("div.post__content");
-            if (query == null)
+            var contentElement = crawledPage.AngleSharpHtmlDocument.QuerySelector("div.post__content");
+            if (contentElement == null)
                 return;
 
-            var title = crawledPage.AngleSharpHtmlDocument.QuerySelector("title");
-            var index = title.TextContent.IndexOf("- lip师兄投资屋");
+            var articleDate = crawledPage.AngleSharpHtmlDocument.QuerySelector("div.post__date").TextContent.Trim();
+            var articleTitle = crawledPage.AngleSharpHtmlDocument.QuerySelector("h1.post__title").TextContent.Trim();
             var articleSource = "";
-            var articleTitle = title.TextContent.Substring(0, index + 1);
-            var article = new WeixinArticle(articleTitle, articleSource, query.InnerHtml);
+            var index = crawledPage.ParentUri.Query.Replace("?page=", "");
+            var page = string.IsNullOrEmpty(index) ? 1 : int.Parse(index);
+            var article = new WeixinArticle(page, articleTitle, articleSource, articleDate, contentElement.InnerHtml);
             lock (lockObject)
             {
                 Articles.Add(article);
@@ -160,43 +231,29 @@ namespace Demo.Windows.ViewModel
             FileStream fileStream = null;
             try
             {
-                articleTitle.Replace("|", "");
-                articleTitle.Replace("<", "");
-                articleTitle.Replace(">", "");
-                articleTitle.Replace(":", "");
-                articleTitle.Replace("?", "");
-                articleTitle.Replace("*", "");
-                articleTitle.Replace("\"", "");
-                articleTitle.Replace("\\", "");
-                articleTitle.Replace("/", "");
-                fileStream = new FileStream(articleTitle + ".html", FileMode.Create);
+                var fileName = articleDate + " - " + articleTitle + ".html";
+                fileName = fileName.Replace("|", "");
+                fileName = fileName.Replace("<", "");
+                fileName = fileName.Replace(">", "");
+                fileName = fileName.Replace(":", "-");
+                fileName = fileName.Replace("?", "");
+                fileName = fileName.Replace("*", "");
+                fileName = fileName.Replace("\"", "");
+                fileName = fileName.Replace("\\", "");
+                fileName = fileName.Replace("/", "");
+                fileStream = new FileStream(Path.Combine(directory, fileName), FileMode.Create);
                 var writer = new StreamWriter(fileStream);
-                writer.Write(query.InnerHtml);
+                writer.Write(contentElement.InnerHtml);
             }
             catch (Exception error)
             {
-
+                AddLog("Article file saving error：" + error.Message);
             }
             finally
             {
                 if (fileStream != null)
                     fileStream.Close();
             }
-
-            //UIService.BeginInvokeIfNeeded(() =>
-            //{
-            //    WebBrowser browser = new WebBrowser();
-            //    browser.Source = crawledPage.Uri;
-            //    browser.LoadCompleted += Browser_LoadCompleted;
-            //});
-        }
-
-        private async void Browser_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
-        {
-            WebBrowser browser = sender as WebBrowser;
-            browser.LoadCompleted -= Browser_LoadCompleted;
-            var parser = new HtmlParser();
-            //var document = await parser.ParseAsync(htmlSourceCode);
         }
 
         void crawler_PageLinksCrawlDisallowed(object sender, PageLinksCrawlDisallowedArgs e)
@@ -222,16 +279,22 @@ namespace Demo.Windows.ViewModel
 
     class WeixinArticle
     {
-        public WeixinArticle(string title, string url, string content)
+        public WeixinArticle(int page, string title, string url, string date, string content)
         {
+            Page = page;
             Title = title;
             OriginUrl = url;
+            Date = date;
             Content = content;
         }
+
+        public int Page { get; }
 
         public string Title { get; }
 
         public string OriginUrl { get; }
+
+        public string Date { get; }
 
         public string Content { get; }
     }
